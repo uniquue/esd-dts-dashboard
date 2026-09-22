aa2added926825e9ecd545e7069f876033fdc5c0
uniquue <spayne1906@gmail.com>
2026-09-22 05:03:31 -0400

    Add latest update card and shorten splash

--- splash.js
+++ splash.js
@@ -1,3 +1,3 @@
 const splash=document.getElementById('splash');
 splash.classList.add('playing');document.body.classList.add('splash-active');
-setTimeout(()=>{splash.remove();document.body.classList.remove('splash-active');},7000);
+setTimeout(()=>{splash.remove();document.body.classList.remove('splash-active');},3000);
